74abed2de5c12d8c80de70f36eef06d5a65d2182
Niranjan Shrestha <niranjansthaa11@gmail.com>
2026-01-30 13:52:52 +0545

    this game has got a moving background after hours of hunting how to do it

--- Assets/Area/Scenes/player.gd
+++ Assets/Area/Scenes/player.gd
@@ -7,16 +7,17 @@ extends CharacterBody2D
 
 
 func _physics_process(delta: float) -> void:
-	# Apply gravity
 	if not is_on_floor():
 		velocity += get_gravity() * delta
+	if Input.is_action_just_pressed("attack"):
+		play_anim("attack")
+	
 	
 
-	# Jump
+	
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = -jump_force
-
-	# Horizontal movement
+	
 	var direction := Input.get_axis("left", "right")
 	if direction != 0:
 		velocity.x = direction * speed
