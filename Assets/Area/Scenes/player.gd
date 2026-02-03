1eb9ac77e0276d5e39aeb2997337d7c088a79960
Niranjan Shrestha <niranjansthaa11@gmail.com>
2026-02-03 21:37:50 +0545

    i made the collison shape more better

--- Assets/Area/Scenes/player.gd
+++ Assets/Area/Scenes/player.gd
@@ -1,51 +1,77 @@
 extends CharacterBody2D
 
+@export_group("Movement")
 @export var speed: float = 200.0
-@export var jump_force: float = 350
-var is_attacking :=false
+@export var jump_force: float = 350.0
+@export var friction: float = 0.25 # How fast the player stops (0 to 1)
 
-@onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
 
+@onready var pivot: Node2D = $Pivot
+@onready var animated_sprite: AnimatedSprite2D = $Pivot/AnimatedSprite2D
+
+## --- State Variables ---
+var is_attacking: bool = false
 
 func _physics_process(delta: float) -> void:
+	# 1. Apply Gravity
 	if not is_on_floor():
 		velocity += get_gravity() * delta
-	if Input.is_action_just_pressed("attack"):
-		play_anim("attack")
+
+	# 2. Handle Inputs
+	if not is_attacking:
+		handle_jump()
+		handle_movement()
+	else:
+		# Apply friction while attacking
+		velocity.x = move_toward(velocity.x, 0, speed * friction)
+
+	# 3. Handle Combat
+	if Input.is_action_just_pressed("attack") and is_on_floor() and not is_attacking:
+		attack()
+
+	# 4. Finalize
+	move_and_slide()
+	update_animation()
+
+
+func handle_jump() -> void:
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = -jump_force
-	
+
+func handle_movement() -> void:
 	var direction := Input.get_axis("left", "right")
+	
 	if direction != 0:
 		velocity.x = direction * speed
-		animated_sprite.flip_h = direction < 0
+		flip_character(direction)
 	else:
-		velocity.x = 0
-	move_and_slide()
+		velocity.x = move_toward(velocity.x, 0, speed * friction)
 
-	update_animation(direction)
+func flip_character(direction: float) -> void:
+	if direction > 0:
+		pivot.scale.x = 1
+	elif direction < 0:
+		pivot.scale.x = -1
 
+func update_animation() -> void:
+	if is_attacking:
+		return
 
-func update_animation(direction: float) -> void:
-	if velocity.y < -20 :
+	if not is_on_floor():
 		play_anim("jump")
-	elif direction != 0:
-		play_anim("run")
 	else:
-		play_anim("idle")
-		
-		
+		if abs(velocity.x) > 0.1:
+			play_anim("run")
+		else:
+			play_anim("idle")
+
 func attack() -> void:
 	is_attacking = true
 	play_anim("attack")
+	
 	await animated_sprite.animation_finished
 	is_attacking = false
-	
 
-
-
-
-func play_anim(name: String) -> void:
-	
-	if animated_sprite.animation != name:
-		animated_sprite.play(name)
+func play_anim(anim_name: String) -> void:
+	if animated_sprite.animation != anim_name:
+		animated_sprite.play(anim_name)
