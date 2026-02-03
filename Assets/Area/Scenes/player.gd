56e959d0da1c853e899a0acd4d14918ca6b389dd
Niranjan Shrestha <niranjansthaa11@gmail.com>
2026-02-03 20:06:30 +0545

    i have made an idea of terrain and made some of the structures in it

--- Assets/Area/Scenes/player.gd
+++ Assets/Area/Scenes/player.gd
@@ -2,6 +2,7 @@ extends CharacterBody2D
 
 @export var speed: float = 200.0
 @export var jump_force: float = 350
+var is_attacking :=false
 
 @onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
 
@@ -11,10 +12,6 @@ func _physics_process(delta: float) -> void:
 		velocity += get_gravity() * delta
 	if Input.is_action_just_pressed("attack"):
 		play_anim("attack")
-	
-	
-
-	
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = -jump_force
 	
@@ -24,14 +21,13 @@ func _physics_process(delta: float) -> void:
 		animated_sprite.flip_h = direction < 0
 	else:
 		velocity.x = 0
-
 	move_and_slide()
 
 	update_animation(direction)
 
 
 func update_animation(direction: float) -> void:
-	if not is_on_floor():
+	if velocity.y < -20 :
 		play_anim("jump")
 	elif direction != 0:
 		play_anim("run")
@@ -40,8 +36,11 @@ func update_animation(direction: float) -> void:
 		
 		
 func attack() -> void:
+	is_attacking = true
 	play_anim("attack")
 	await animated_sprite.animation_finished
+	is_attacking = false
+	
 
 
 
